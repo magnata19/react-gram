f79836574b3934fc68f4714dd569d00327cf5586
Davidson <dehpacifico@gmail.com>
2023-12-22 13:20:15 -0300

    criando arquitetura mvc e rota de teste

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
+require('dotenv').config()
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 
 const app = express();
 
 //config JSON and form data response
 app.use(express.json()); //para receber respostas em formato de texto
 app.use(express.urlencoded({ extended: false })); // para aceitar form data
+
+//Routes
+const router = require('./routes/Router.js')
+app.use(router)
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`)
