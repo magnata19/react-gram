8efc671894775aab7c8f30d345d264572de7b7eb
Davidson <dehpacifico@gmail.com>
2023-12-22 18:49:26 -0300

    conectando ao banco de dados

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
-//connection
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.qb5hajw.mongodb.net/?retryWrites=true&w=majority`)
+    console.log('conectou ao banco com sucesso')
+    return dbConn
+  } catch (error) {
+    console.error(error, 'deu ruim na conexao do db')
+  }
+}
+
+conn();
+
+module.exports = conn;
