5f5d0e054f233e25c2a7c61d528954df94dc5750
Davidson <dehpacifico@gmail.com>
2023-12-25 13:59:22 -0300

    criando funcao para alterar nome, bio e imagem

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+const mongoose = require('mongoose');
 const jwtSecret = process.env.JWT_SECRET;
 
 //Generate user token
@@ -79,8 +81,47 @@ const getCurrentUser = async(req, res) => {
   res.status(200).json(user)
 }
 
+//update an user
+const update = async(req, res) => {
+  const {name, password, bio} = req.body
+  let profileImage = null;
+
+  if(req.file){
+    profileImage = req.file.filename
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id))
+
+  if(name){
+    user.name = name
+  }
+
+  if(password) {
+    //generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(passoword, salt)
+
+    user.passoword = passwordHash;
+  }
+
+  if(profileImage){
+    user.profileImage = profileImage
+  }
+
+  if(bio) {
+    user.bio = bio;
+  }
+
+  await user.save()
+
+  res.status(200).json(user)
+}
+
 module.exports = {
   register,
   login,
-  getCurrentUser
+  getCurrentUser,
+  update
 };
