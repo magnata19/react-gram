460c4e13c60b893680c40e599ba050e66a4ca18b
Davidson <dehpacifico@gmail.com>
2024-01-04 21:55:23 -0300

    criando estilizacoes das paginas/ pagina de registro

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,8 +1,30 @@
 import './Auth.css'
 
+//components
+import {Link} from 'react-router-dom'
+
+//hooks
+import { useState, useEffect } from 'react'
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div id='register'>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Nome'/>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='Confirme sua senha' />
+        <input type="submit" value='Cadastrar'/>
+      </form>
+      <p>Já tem conta? <Link to='/login'>Clique aqui!</Link></p>
+    </div>
   )
 }
 
