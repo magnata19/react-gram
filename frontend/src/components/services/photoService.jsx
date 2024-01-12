ca04d2b9781c3ff88a34804cc53991cd823d7aab
Davidson <dehpacifico@gmail.com>
2024-01-11 21:16:28 -0300

    criando metodos dentro de services e slices de fotos

--- frontend/src/components/services/photoService.jsx
+++ frontend/src/components/services/photoService.jsx
@@ -1,7 +1,21 @@
 import { api, requestConfig } from "../utils/config";
 
+//publish an user photo
+const publishPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
 
+  try {
+    const res = await fetch(`${api}/photos`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const photoService = {};
+const photoService = {
+  publishPhoto,
+};
 
 export default photoService;
--- frontend/src/components/slices/photoSlice.jsx
+++ frontend/src/components/slices/photoSlice.jsx
@@ -7,18 +7,54 @@ const initialState = {
   error: false,
   success: false,
   loading: false,
-  message: null
-}
+  message: null,
+};
+
+//publish user photo
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.publishPhoto(photo, token);
+
+    //check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
 
 const photoSlice = createSlice({
-  name:'photo',
+  name: "photo",
   initialState,
   reducers: {
     resetMessage: (state) => {
       state.message = null;
-    }
-  }
-})
+    },
+  },
+  extraReducers: (builder) =>
+    builder
+      .addCase(publishPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
+        state.photo.unshift(state.photo)
+        state.message = 'Foto publicada com sucesso!    '
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      }),
+});
 
-export const {resetMessage} = photoSlice.actions;
+export const { resetMessage } = photoSlice.actions;
 export default photoSlice.reducer;
