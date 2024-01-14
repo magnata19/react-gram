fe4fb729d401afb6ead324fefcf915220610ba4a
Davidson <dehpacifico@gmail.com>
2024-01-14 13:37:59 -0300

    estilizando pagina de foto e criando service e slice para dar like na foto

--- frontend/src/components/slices/photoSlice.jsx
+++ frontend/src/components/slices/photoSlice.jsx
@@ -73,11 +73,21 @@ export const updatePhoto = createAsyncThunk(
 );
 
 //get photo by id
-export const getPhoto = createAsyncThunk("photo/getphoto", async (id, thunkAPI) => {
+export const getPhoto = createAsyncThunk(
+  "photo/getphoto",
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
 
+    const data = await photoService.getPhoto(id, token);
+    return data;
+  }
+);
+
+//like a photo
+export const like = createAsyncThunk("photo/like", async (id, thunkAPI) => {
   const token = thunkAPI.getState().auth.user.token;
 
-  const data = await photoService.getPhoto(id, token);
+  const data = await photoService.like(id, token);
   return data;
 });
 
@@ -166,6 +176,25 @@ const photoSlice = createSlice({
         state.success = true;
         state.error = null;
         state.photo = action.payload;
+      }).addCase(like.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        if(state.photo.likes) {
+          state.photo.likes.push(action.payload.userId);
+        }
+
+        state.photos.map((photo) => {
+          if (photo._id === action.payload.photo.photoId) {
+            return state.photo.likes.push(action.payload.userId);
+          }
+          return photo;
+        });
+        state.message = action.payload.message;
+      })
+      .addCase(like.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
       }),
 });
 
